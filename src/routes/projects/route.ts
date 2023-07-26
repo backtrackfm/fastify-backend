04a573980c3201acf3cf846bda81548fd7fd4359
tika <48658947+tika@users.noreply.github.com>
2023-07-26 23:02:39 +0200

    feat: validate fields when processing creation route

--- src/routes/projects/route.ts
+++ src/routes/projects/route.ts
@@ -1,10 +1,10 @@
 import { FastifyInstance, RouteOptions } from "fastify";
 import { redirectToLogin } from "../../lib/auth";
 import { createProjectSchema } from "../../schema/projectsSchema";
-import { pump, s3 } from "../../server";
-import fs from "fs";
+import { s3 } from "../../server";
 import { MultipartFile } from "@fastify/multipart";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { stdReply } from "../../lib/std-reply";
 
 export default async function routes(
   fastify: FastifyInstance,
@@ -19,23 +19,21 @@ export default async function routes(
       const parts = request.parts();
       let rawTextDetails: any = {};
       const coverArtFieldname = "coverArt";
-      // let coverArtPart: MultipartFile | undefined;
+      let coverArtPart: MultipartFile | undefined;
+      let coverArtBuffer;
 
       for await (const part of parts) {
         if (part.type !== "file") {
           rawTextDetails[part.fieldname] = part.value;
         } else {
           if (part.fieldname === coverArtFieldname) {
-            const coverArt = part;
-
-            const putObjectCommand = new PutObjectCommand({
-              Bucket: process.env.AWS_BUCKET,
-              Key: coverArt.filename,
-              Body: await coverArt.toBuffer(),
-              ContentType: coverArt.mimetype,
-            });
-
-            await s3.send(putObjectCommand);
+            coverArtPart = part;
+            coverArtBuffer = await part.toBuffer();
+          } else {
+            // Can we do something better?
+            // Note: we MUST consume all parts
+            // From: https://github.com/fastify/fastify-multipart
+            await part.toBuffer();
           }
         }
       }
@@ -43,8 +41,21 @@ export default async function routes(
       // zod parse these text details
       const details = await createProjectSchema.parseAsync(rawTextDetails);
 
-      reply.send({
-        ok: details,
+      // If there's a cover art, upload it to the cloud
+      if (coverArtPart) {
+        const putObjectCommand = new PutObjectCommand({
+          Bucket: process.env.AWS_BUCKET,
+          Key: coverArtPart.filename,
+          Body: coverArtBuffer,
+          ContentType: coverArtPart.mimetype,
+        });
+
+        await s3.send(putObjectCommand);
+      }
+
+      stdReply(reply, {
+        data: details,
+        clientMessage: `Created project ${details.name}`,
       });
     }
   );
--- src/schema/projectsSchema.ts
+++ src/schema/projectsSchema.ts
@@ -4,7 +4,32 @@ import { STD_STRING } from "./schemaUtils";
 export const createProjectSchema = z.object({
   genre: STD_STRING,
   name: STD_STRING,
-  tags: z.string().min(1).optional(), // would be array but fd doesn't allow anything other than strings.
-  // coverArtURL: .file().optional(),
+  tags: z
+    .string()
+    .min(1)
+    .refine(
+      (value) => {
+        try {
+          const parsedValue = JSON.parse(value);
+          return (
+            Array.isArray(parsedValue) &&
+            parsedValue.every((item) => typeof item === "string")
+          );
+        } catch {
+          return false;
+        }
+      },
+      {
+        message: "Invalid JSON-formatted string array",
+      }
+    )
+    .transform((value) => {
+      try {
+        return JSON.parse(value);
+      } catch (error) {
+        throw new Error("Failed to parse JSON-formatted string array");
+      }
+    })
+    .optional(), // would be array but fd doesn't allow anything other than strings.
   description: STD_STRING.optional(),
 });
