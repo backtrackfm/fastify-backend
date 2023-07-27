53f6487b8469f69f997a46d6651e100a35605a9e
tika <48658947+tika@users.noreply.github.com>
2023-07-27 22:26:39 +0200

    feat: update branch

--- src/schema/branchesSchema.ts
+++ src/schema/branchesSchema.ts
@@ -5,3 +5,8 @@ export const createBranchSchema = z.object({
   name: STD_STRING,
   description: STD_STRING.optional(),
 });
+
+export const updateBranchSchema = z.object({
+  name: STD_STRING.optional(),
+  description: STD_STRING.optional(),
+});
