5a5ec5a26fd5cce869fe51c58248dbf22cfa5aa4
tika <48658947+tika@users.noreply.github.com>
2023-07-27 22:00:36 +0200

    feat: get branch by id

--- src/routes/projects/_projectId/branches/_branchId/route.ts
+++ src/routes/projects/_projectId/branches/_branchId/route.ts
@@ -12,11 +12,51 @@ export default async function routes(
       preValidation: (request, reply) => redirectToLogin(request, reply),
     },
     async (request, reply) => {
-      // const { projectId } =
+      const { branchId, projectId } = request.params as {
+        projectId: string;
+        branchId: string;
+      };
 
       if (!request.user) {
         return stdReply(reply, stdNoAuth);
       }
+
+      const branch = await fastify.prisma.branch.findFirst({
+        where: {
+          id: branchId,
+          projectId: projectId,
+        },
+        include: {
+          project: true,
+        },
+      });
+
+      if (!branch) {
+        return stdReply(reply, {
+          error: {
+            code: 400,
+            type: "not-found",
+          },
+          clientMessage: `No branch ${branchId} found`,
+        });
+      }
+
+      const { project, ...rest } = branch;
+
+      if (branch.project.createdByUserId !== request.user.id) {
+        return stdReply(reply, {
+          error: {
+            code: 400,
+            type: "validation",
+            details: `${request.user.id} !== ${branch.project.createdByUserId}`,
+          },
+          clientMessage: "You can only get branches from your own projects",
+        });
+      }
+
+      return stdReply(reply, {
+        data: rest,
+      });
     }
   );
 }
