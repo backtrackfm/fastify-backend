99ff891ac6f01534a6f8490d9d22919ccf43f298
tika <48658947+tika@users.noreply.github.com>
2023-08-12 22:47:10 +0100

    feat: add scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/express-app
+#give permission for everything in the backtrack-backend directory
+sudo chmod -R 777 /home/ec2-user/backtrack-backend
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/express-app
+cd /home/ec2-user/backtrack-backend
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"
