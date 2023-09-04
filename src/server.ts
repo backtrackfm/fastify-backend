a00d28696f824ca45118f28d6d835dfcd8889ca4
tika <48658947+tika@users.noreply.github.com>
2023-09-04 18:23:06 +0100

    del: cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import { S3Client } from "@aws-sdk/client-s3";
 import autoload from "@fastify/autoload";
-import cors from "@fastify/cors";
 import fastifyFormbody from "@fastify/formbody";
 import fastifyMultipart from "@fastify/multipart";
 import fastifyPassport from "@fastify/passport";
@@ -41,10 +40,12 @@ export const s3 = new S3Client({
 app.register(prismaPlugin);
 app.register(fastifyFormbody);
 app.register(fastifyMultipart);
-app.register(cors, {
-  origin: ["http://localhost:8888", "app://."],
-  credentials: true,
-});
+// app.register(cors, {
+//   origin: ["http://localhost:8888", "app://."],
+//   credentials: true,
+//   allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
+//   methods: "GET, POST, PATCH, DELETE",
+// });
 
 // Sessions
 app.register(fastifySecureSession, {
